bd2f29707175d4d785c2cbe976f25033e824d700
Alex Shabalin <shabalin.media@gmail.com>
2022-07-28 23:51:12 +0600

    Added First Fuctionality.

---
+++ Tipsy/Controllers/ResultViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by Alexander Shabalin on 28/7/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
